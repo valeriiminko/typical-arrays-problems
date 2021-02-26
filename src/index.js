bae91f0e4eae98f95224c65a62cea7e571bab73d
valeriiminko <50117001+valeriiminko@users.noreply.github.com>
2021-02-26 03:57:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array && array.length > 0) ? array.reduce((prev, curr) => prev = prev > curr ? curr: prev, Infinity): 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array && array.length > 0) ? array.reduce((prev, curr) => prev = prev < curr ? curr: prev, -Infinity): 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (array && array.length > 0) ? array.reduce((prev, curr) => prev + curr, 0) / array.length: 0;
 }
